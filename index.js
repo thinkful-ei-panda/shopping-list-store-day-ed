c5b5ddabef7680e84165d10e3fcc328c2df8c366
A <jesus.e.estrada@hotmail.com>
2020-06-05 15:28:54 -0500

    added new handles

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const store = {
   items: [
     { id: cuid(), name: 'apples', checked: false },
@@ -8,6 +10,14 @@ const store = {
   hideCheckedItems: false
 };
 
+
+/*
+uses 'item' as the overall index to 'items'. Goes through each index in items array
+creates new array with html
+Generates a strike-through taken from index.css
+if true, itemTittle in return below is given strike-through CSS with class='shopping-item shopping-item__checked'
+if false, itemTitle in return is given no strike through in return below
+*/
 const generateItemElement = function (item) {
   let itemTitle = `<span class='shopping-item shopping-item__checked'>${item.name}</span>`;
   if (!item.checked) {
@@ -30,6 +40,18 @@ const generateItemElement = function (item) {
     </li>`;
 };
 
+
+/*
+THERE ARE MULTIPLE DIVS IN AN ARRAY CREATED BY FUNCTION ABOVE
+how to put them all together in DOM inside of <ul shopping-list js-shopping-list></ul>?
+use .map() and .join('') to get them together as a string
+*/
+
+/*
+generateShoppingItemsString function using each index created in generateItemElement
+creates new array called items using shoppingList (generateItemElement) array
+returns items indexes back together as a string with .join('')
+*/
 const generateShoppingItemsString = function (shoppingList) {
   const items = shoppingList.map((item) => generateItemElement(item));
   return items.join('');
@@ -66,6 +88,12 @@ const addItemToShoppingList = function (itemName) {
   store.items.push({ id: cuid(), name: itemName, checked: false });
 };
 
+
+/*
+Takes in value (.js-shopping-list-entry) using .val()
+Converts it into variable name 'newItemName'
+Will still add if entry is empty. Can be fixed if given .length value > 1?
+*/
 const handleNewItemSubmit = function () {
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault();
@@ -88,6 +116,7 @@ const handleItemCheckClicked = function () {
     render();
   });
 };
+
 
 const getItemIdFromElement = function (item) {
   return $(item)
@@ -127,6 +156,10 @@ const handleDeleteItemClicked = function () {
   });
 };
 
+const editListItemName = function (id, itemName) {
+  const item = store.items.find(item => item.id === id);
+  item.name = itemName;
+};
 /**
  * Toggles the store.hideCheckedItems property
  */
@@ -145,6 +178,16 @@ const handleToggleFilterClick = function () {
   });
 };
 
+const handleEditShoppingItemSubmit = function () {
+  $('.js-shopping-list').on('submit', '.js-edit-item', event => {
+    event.preventDefault();
+    const id = getItemIdFromElement(event.currentTarget);
+    const itemName = $(event.currentTarget).find('.shopping-item').val();
+    editListItemName(id, itemName);
+    render();
+  });
+};
+
 /**
  * This function will be our callback when the
  * page loads. It is responsible for initially 
@@ -159,6 +202,7 @@ const handleShoppingList = function () {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
+  handleEditShoppingItemSubmit();
   handleToggleFilterClick();
 };
 
